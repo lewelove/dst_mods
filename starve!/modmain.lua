362755b7834cf3253664f886d4c1cc5f00503150
lewelove <lewelove@proton.me>
2026-04-26 13:07:23 +0300

    2026-04-26 at 10:07 UTC

--- starve!/modmain.lua
+++ starve!/modmain.lua
@@ -5,58 +5,58 @@ local PLANT_DEFS = require("prefabs/farm_plant_defs").PLANT_DEFS
 
 local SPICES = { "_spice_garlic", "_spice_sugar", "_spice_chili", "_spice_salt" }
 
-local function ChangeStats(prefab, h, hu, s)
+local function ChangeStats(prefab, hu, s, h)
     AddPrefabPostInit(prefab, function(inst)
         if inst ~= nil and inst.components.edible ~= nil then
-            inst.components.edible.healthvalue = h
             inst.components.edible.hungervalue = hu
             inst.components.edible.sanityvalue = s
+            inst.components.edible.healthvalue = h
         end
     end)
     
     for _, spice in ipairs(SPICES) do
         AddPrefabPostInit(prefab .. spice, function(inst)
             if inst ~= nil and inst.components.edible ~= nil then
-                inst.components.edible.healthvalue = h
                 inst.components.edible.hungervalue = hu
                 inst.components.edible.sanityvalue = s
+                inst.components.edible.healthvalue = h
             end
         end)
     end
 end
 
-ChangeStats("asparagus", 3, 12.5, 0)
-ChangeStats("asparagus_cooked", 8, 12.5, 0)
-ChangeStats("cactus_meat", -10, 9.375, -10)
-ChangeStats("cactus_meat_cooked", 0, 12.5, 10)
-ChangeStats("corn", 3, 12.5, 0)
-ChangeStats("corn_cooked", 1, 9.375, 0)
-ChangeStats("durian", -10, 12.5, -10)
-ChangeStats("durian_cooked", 0, 18.75, -20)
-ChangeStats("eggplant", 3, 12.5, 0)
-ChangeStats("eggplant_cooked", 8, 18.75, 0)
-ChangeStats("berries_juicy_cooked", 8, 12.5, 0)
-ChangeStats("potato", -3, 9.375, -5)
-ChangeStats("potato_cooked", 3, 18.75, 0)
-ChangeStats("pumpkin", 3, 18.75, 0)
-ChangeStats("pumpkin_cooked", 8, 25, 0)
-ChangeStats("tomato", 3, 12.5, 0)
-ChangeStats("tomato_cooked", 8, 12.5, 0)
+ChangeStats("asparagus", 12.5, 0, 3)
+ChangeStats("asparagus_cooked", 12.5, 0, 8)
+ChangeStats("cactus_meat", 9.375, -10, -10)
+ChangeStats("cactus_meat_cooked", 12.5, 10, 0)
+ChangeStats("corn", 12.5, 0, 3)
+ChangeStats("corn_cooked", 9.375, 0, 1)
+ChangeStats("durian", 12.5, -10, -10)
+ChangeStats("durian_cooked", 18.75, -20, 0)
+ChangeStats("eggplant", 12.5, 0, 3)
+ChangeStats("eggplant_cooked", 18.75, 0, 8)
+ChangeStats("berries_juicy_cooked", 12.5, 0, 8)
+ChangeStats("potato", 9.375, -5, -3)
+ChangeStats("potato_cooked", 18.75, 0, 3)
+ChangeStats("pumpkin", 18.75, 0, 3)
+ChangeStats("pumpkin_cooked", 25, 0, 8)
+ChangeStats("tomato", 12.5, 0, 3)
+ChangeStats("tomato_cooked", 12.5, 0, 8)
 
-ChangeStats("cookedmonstermeat", -10, 18.75, -10)
-ChangeStats("seeds", 0, 2.35, 0)
-ChangeStats("seeds_cooked", 1, 2.35, 0)
-ChangeStats("butterflywings", 3, 4.7, 0)
+ChangeStats("cookedmonstermeat", 18.75, -10, -10)
+ChangeStats("seeds", 2.35, 0, 0)
+ChangeStats("seeds_cooked", 2.35, 0, 1)
+ChangeStats("butterflywings", 4.7, 0, 3)
 
-ChangeStats("butterflymuffin", 20, 25, 5)
-ChangeStats("dragonpie", 20, 75, 5)
-ChangeStats("fishtacos", 25, 25, 15)
-ChangeStats("meatballs", 3, 47.5, 5)
-ChangeStats("bonestew", 12, 112.5, 5)
-ChangeStats("pumpkincookie", 0, 37.5, 33)
-ChangeStats("asparagussoup", 30, 18.75, 5)
-ChangeStats("potatotornado", 3, 18.75, 15)
-ChangeStats("vegstinger", 3, 18.75, 33)
+ChangeStats("butterflymuffin", 25, 5, 20)
+ChangeStats("dragonpie", 75, 5, 20)
+ChangeStats("fishtacos", 25, 15, 25)
+ChangeStats("meatballs", 47.5, 5, 3)
+ChangeStats("bonestew", 112.5, 5, 12)
+ChangeStats("pumpkincookie", 37.5, 33, 0)
+ChangeStats("asparagussoup", 18.75, 5, 30)
+ChangeStats("potatotornado", 18.75, 15, 3)
+ChangeStats("vegstinger", 18.75, 33, 3)
 
 local function FundamentalWinterFreeze(inst)
     if not GLOBAL.TheWorld.ismastersim then return end
---
+++ sync.sh
@@ -0,0 +1,1 @@
+rsync -av --delete '/home/lewelove/dev/dst_mods/starve!/' '/home/lewelove/.local/share/Steam/steamapps/common/Don'\''t Starve Together/mods/starve!/'
