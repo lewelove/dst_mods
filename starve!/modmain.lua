95db7193366475522ce4742bc213fc89f492ebdc
lewelove <lewelove@proton.me>
2026-05-08 00:41:57 +0300

    2026-05-07 at 21:41 UTC

--- starve!/modmain.lua
+++ starve!/modmain.lua
@@ -32,50 +32,265 @@ local function ChangeStats(prefab, hu, s, h)
     end
 end
 
-ChangeStats("asparagus", 12.5, 0, 3)
+
+---- Veggies ----
+
+-- Cooked Asparagus
+-- 25, 0, 3 -> rebalance
 ChangeStats("asparagus_cooked", 12.5, 0, 8)
-ChangeStats("acorn_cooked", 2.375, 0, 1)
-ChangeStats("butterflywings", 4.675, 0, 3)
+
+-- Cactus Flesh
+-- 12.5, -5, -3 -> nerf
 ChangeStats("cactus_meat", 9.375, -10, -10)
-ChangeStats("cactus_meat_cooked", 12.5, 10, 0)
+
+-- Cooked Cactus Flesh
+-- 12.5, 15, 1 -> nerf
+ChangeStats("cactus_meat_cooked", 12.5, 10, 1)
+
+-- Corn
+-- 25, 0, 3 -> nerf
 ChangeStats("corn", 12.5, 0, 3)
+
+-- Popcorn
+-- 12.5, 0, 3 -> nerf
 ChangeStats("corn_cooked", 9.375, 0, 1)
-ChangeStats("durian", 12.5, -10, -10)
-ChangeStats("durian_cooked", 18.75, -20, 0)
+
+-- Eggplant
+-- 25, 0, 8 -> nerf
 ChangeStats("eggplant", 12.5, 0, 3)
+
+-- Braised Eggplant
+-- 25, 0, 20 -> nerf
 ChangeStats("eggplant_cooked", 18.75, 0, 8)
-ChangeStats("berries_juicy_cooked", 12.5, 0, 8)
-ChangeStats("moon_cap", 0, 10, 0)
-ChangeStats("moon_cap_cooked", 0, -10, 0)
-ChangeStats("blue_cap", 0, -15, 20)
-ChangeStats("blue_cap_cooked", 0, 10, -3)
-ChangeStats("green_cap", 0, -50, 0)
-ChangeStats("green_cap_cooked", 0, 15, -1)
-ChangeStats("red_cap", 0, 0, -20)
-ChangeStats("red_cap_cooked", 0, -10, 1)
-ChangeStats("potato", 9.375, -5, -3)
-ChangeStats("potato_cooked", 18.75, 0, 3)
-ChangeStats("pumpkin", 18.75, 0, 3)
-ChangeStats("pumpkin_cooked", 25, 0, 8)
-ChangeStats("tomato", 12.5, 0, 3)
+
+-- Mandrake
+-- 75, 0, 60 -> made inedible
+
+-- Potato
+-- 12.5, -5, -3 -> nerf
+ChangeStats("potato", 4.675, -5, -3)
+
+-- Roasted Potato
+-- 25, 0, 20 -> nerf
+ChangeStats("potato_cooked", 12.5, 0, 3)
+
+-- Pumpkin
+-- 37.5, 0, 3 -> nerf
+ChangeStats("pumpkin", 12.5, 0, 3)
+
+-- Hot Pumpkin
+-- 37.5, 0, 8 -> nerf
+ChangeStats("pumpkin_cooked", 18.75, 0, 8)
+
+-- Roasted Toma Root
+-- 12.5, 0, 20 -> nerf
 ChangeStats("tomato_cooked", 12.5, 0, 8)
 
+-- Moon Shroom
+-- 12.5, 10, 0 -> rebalance
+ChangeStats("moon_cap", 0, 20, 0)
+
+-- Cooked Moon Shroom
+-- -12.5, -10, 0 -> rebalance
+ChangeStats("moon_cap_cooked", 0, -20, 0)
+
+-- Blue Cap
+-- 12.5, -15, 20 -> nerf
+ChangeStats("blue_cap", 0, -15, 20)
+
+-- Cooked Blue Cap
+-- 12.5, 10, -3 -> nerf
+ChangeStats("blue_cap_cooked", 0, 10, -3)
+
+-- Green Cap
+-- 12.5, -50, 0 -> nerf
+ChangeStats("green_cap", 0, -50, 0)
+
+-- Cooked Green Cap
+-- 0, 15, -1 -> nerf
+ChangeStats("green_cap_cooked", 0, 15, -3)
+
+-- Red Cap
+-- 12.5, 0, -20 -> nerf
+ChangeStats("red_cap", 0, 0, -20)
+
+-- Cooked Red Cap
+-- 0, -10, 1 -> buff
+ChangeStats("red_cap_cooked", 0, -10, 8)
+
+
+
+---- Fruits ----
+
+-- Durian
+-- 25, -5, -3 -> nerf
+ChangeStats("durian", 12.5, -20, -20)
+
+-- Extra Smelly Durian
+-- 25, -5, 0 -> nerf
+ChangeStats("durian_cooked", 18.75, -20, 0)
+
+-- Sliced Pomegranate
+-- 12.5, 0, 20 -> nerf
+ChangeStats("pomegranate_cooked", 12.5, 0, 8)
+
+-- Roasted Juicy Berries
+-- 18.75, 0, 3 -> rebalance
+ChangeStats("berries_juicy_cooked", 12.5, 0, 8)
+
+
+
+---- Meats ----
+
+-- Monster Meat
+-- 18.75, -15, -20 -> nerf
+ChangeStats("monstermeat", 12.5, -10, -10)
+
+-- Cooked Monster Meat
+-- 18.75, -10, -3 -> nerf
 ChangeStats("cookedmonstermeat", 18.75, -10, -10)
+
+-- Monster Jerky
+-- 18.75, -5, -3 -> nerf
+ChangeStats("monstermeat_dried", 12.5, -5, -3)
+
+-- Meat
+-- 25, -10, 1 -> nerf
+ChangeStats("meat", 18.75, -10, 1)
+
+-- Jerky
+-- 25, 15, 20 -> nerf
+ChangeStats("meat_dried", 18.75, 15, 20)
+
+-- Drumstick
+-- 12.5, -10, 0 -> nerf
+ChangeStats("drumstick", 9.375, -10, 0)
+
+-- Frog Legs
+-- 12.5, -10, 0 -> nerf
+ChangeStats("froglegs", 9.375, 10, 0)
+
+-- Morsel
+-- 12.5, -10, 0 -> nerf
+ChangeStats("smallmeat", 9.375, 10, 0)
+
+-- Small Jerky
+-- 12.5, 10, 8 -> nerf
+ChangeStats("smallmeat_dried", 9.375, 10, 8)
+
+
+
+---- Other ----
+
+-- Roasted Birchnut
+-- 9.375, 0, 1 -> nerf
+ChangeStats("acorn_cooked", 2.375, 0, 1)
+
+-- Butterfly Wings
+-- 9.375, 0, 8 -> nerf
+ChangeStats("butterflywings", 4.375, 0, 3)
+
+-- Moon Moth Wings
+-- 9.375, 15, 8 -> nerf
+ChangeStats("moonbutterflywings", 4.375, 15, 3)
+
+-- Seeds
+-- 4.6875, 0, 0 -> nerf
 ChangeStats("seeds", 2.375, 0, 0)
+
+-- Toasted Seeds
+-- 4.6875, 0, 1 -> nerf
 ChangeStats("seeds_cooked", 2.375, 0, 1)
 
+-- Crop Seeds
+-- 9.375, 0, 0.5 -> nerf
+ChangeStats("carrot_seeds", 2.375, 0, 0)
+ChangeStats("corn_seeds", 2.375, 0, 0)
+ChangeStats("dragonfruit_seeds", 2.375, 0, 0)
+ChangeStats("durian_seeds", 2.375, 0, 0)
+ChangeStats("eggplant_seeds", 2.375, 0, 0)
+ChangeStats("pomegranate_seeds", 2.375, 0, 0)
+ChangeStats("pumpkin_seeds", 2.375, 0, 0)
+ChangeStats("watermelon_seeds", 2.375, 0, 0)
+ChangeStats("asparagus_seeds", 2.375, 0, 0)
+ChangeStats("tomato_seeds", 2.375, 0, 0)
+ChangeStats("potato_seeds", 2.375, 0, 0)
+ChangeStats("onion_seeds", 2.375, 0, 0)
+ChangeStats("pepper_seeds", 2.375, 0, 0)
+ChangeStats("garlic_seeds", 2.375, 0, 0)
+
+
+---- Dishes ----
+
+-- Asparagus Soup
+-- 18.75, 5, 20 -> buff -> reason: unsed
 ChangeStats("asparagussoup", 18.75, 5, 30)
+
+-- Banana Pop
+-- 12.5, 33, 20 -> nerf -> reason: allowing ice
+ChangeStats("bananapop", 12.5, 33, 0)
+
+-- Banana Shake
+-- 25, 33, 8 -> buff -> reason: unused
+ChangeStats("bananajuice", 25, 33, 20)
+
+-- Bunny Stew
+-- 37.5, 5, 20 -> nerf -> reason: allowing ice
+ChangeStats("bunnystew", 12.5, 5, 20)
+
+-- Butter Muffin
+-- 37.5, 5, 20 -> nerf -> reason: OP + butterfly wings are nerfed
 ChangeStats("butterflymuffin", 12.5, 5, 20)
-ChangeStats("dragonpie", 18.75, 5, 40)
+
+-- Ceviche
+-- 25, 5, 20 -> buff -> reason: unused
+ChangeStats("ceviche", 37.5, 5, 40)
+
+-- Fancy Spiralled Tubers
+-- 37.5, 15, 3 -> nerf -> reason: OP + potato nerf
 ChangeStats("potatotornado", 18.75, 15, 3)
+
+-- Fish Tacos
+-- 37.5, 5, 20 -> rebalance -> reason: unused
+ChangeStats("fishtacos", 18.75, 15, 20)
+
+-- Fist Full of Jam
+-- 37.5, 5, 3 -> nerf -> reason: allowing ice
+ChangeStats("jammypreserves", 18.75, 5, 3)
+
+-- Frozen Banana Daiquiri
+-- 18.75, 15, 30 -> buff -> reason: unused
+ChangeStats("frozenbananadaiquiri", 18.75, 15, 40)
+
+-- Meaty Stew
+-- 150, 5, 12 -> nerf -> reason: OP + allowing ice
+ChangeStats("bonestew", 92.5, 5, 12)
+
+-- Monster Lasagna
+-- 37.5, -20, -20 -> buff -> reason: global monster meat ban
+ChangeStats("monsterlasagna", 62.5, -20, -20)
+
+-- Pumpkin Cookie
+-- 37.5, 15, 0 -> rebalance -> reason: unused
 ChangeStats("pumpkincookie", 18.75, 33, 0)
+
+-- Salsa Fresca
+-- 25, 33, 3 -> nerf -> reason: allowing ice
 ChangeStats("salsa", 18.75, 33, 3)
-ChangeStats("vegstinger", 18.75, 33, 3)
+
+-- Trail Mix
+-- 12.5, 5, 30 -> nerf -> reason: OP
 ChangeStats("trailmix", 12.5, 5, 20)
 
-ChangeStats("fishtacos", 18.75, 15, 20)
-ChangeStats("meatballs", 47.5, 5, 3)
-ChangeStats("bonestew", 112.5, 5, 12)
+-- Vegetable Stinger
+-- 25, 33, 3 -> nerf -> reason: allowing ice
+ChangeStats("vegstinger", 18.75, 33, 3)
+
+
+-- -- 
+-- -- , ,  -> nerf buff -> reason: 
+-- ChangeStats("", , , )
+
 
 local cooking = GLOBAL.require("cooking")
 
@@ -94,21 +309,35 @@ local MONSTER_MEAT_BAN_LIST = {
     pepperpopper = true,
     surfnturf = true,
     turkeydinner = true,
-    -- ICE_ALLOW ban so they don't steal the priority
+    -- ban of dishes with allowed ice, so they don't steal the priority
+    bunnystew = true,
     ceviche = true,
+    guacamole = true,
     lobsterbisque = true,
+    mandrakesoup = true,
+    bonestew = true,
+    seafoodgumbo = true,
     asparagugazpacho = true,
 }
 
 local ICE_ALLOW_LIST = {
+    asparagugazpacho = true,
+    asparagussoup = true,
     bananapop = true,
     bunnystew = true,
     ceviche = true,
+    jammypreserves = true,
     frozenbananadaiquiri = true,
+    guacamole = true,
     icecream = true,
     lobsterbisque = true,
+    mandrakesoup = true,
+    bonestew = true,
     melonsicle = true,
-    asparagugazpacho = true,
+    salsa = true,
+    seafoodgumbo = true,
+    sweettea = true,
+    vegstinger = true,
 }
 
 local function ApplyCrockPotRestrictions()
