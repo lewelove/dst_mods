5b3be0a88f50990b517f4a8dba0004b8e2cf1b8b
lewelove <lewelove@proton.me>
2026-06-16 17:40:59 +0300

    2026-06-16 at 14:40 UTC

--- starve!/modmain.lua
+++ starve!/modmain.lua
@@ -73,10 +73,6 @@ ChangeStats("eggplant_cooked", 18.75, 0, 8)
 -- 75, 0, 60 -> made inedible
 AddPrefabPostInit("mandrake", DisableEating)
 
--- Cooked Mandrake
--- 150, 0, 100 -> made inedible
-AddPrefabPostInit("mandrake_cooked", DisableEating)
-
 -- Potato
 -- 12.5, -5, -3 -> nerf
 ChangeStats("potato", 4.675, -5, -3)
@@ -141,6 +137,14 @@ ChangeStats("durian", 12.5, -20, -20)
 -- 25, -5, 0 -> nerf
 ChangeStats("durian_cooked", 18.75, -20, 0)
 
+-- Dragon Fruit 
+-- 9.375, 0, 3 -> no changes
+ChangeStats("dragonfruit", 9.375, 0, 3)
+
+-- Prepared Dragon Fruit 
+-- 12.5, 0, 20 -> nerf
+ChangeStats("dragonfruit", 12.5, 0, 8)
+
 -- Sliced Pomegranate
 -- 12.5, 0, 20 -> nerf
 ChangeStats("pomegranate_cooked", 12.5, 0, 8)
@@ -155,7 +159,7 @@ ChangeStats("berries_juicy_cooked", 12.5, 0, 8)
 
 -- Monster Meat
 -- 18.75, -15, -20 -> nerf
-ChangeStats("monstermeat", 12.5, -10, -10)
+ChangeStats("monstermeat", 18.75, -20, -20)
 
 -- Cooked Monster Meat
 -- 18.75, -10, -3 -> nerf
@@ -163,31 +167,31 @@ ChangeStats("cookedmonstermeat", 18.75, -10, -10)
 
 -- Monster Jerky
 -- 18.75, -5, -3 -> nerf
-ChangeStats("monstermeat_dried", 12.5, -5, -3)
+-- ChangeStats("monstermeat_dried", 12.5, -5, -3)
 
 -- Meat
 -- 25, -10, 1 -> nerf
-ChangeStats("meat", 18.75, -10, 1)
+-- ChangeStats("meat", 18.75, -10, 1)
 
 -- Jerky
 -- 25, 15, 20 -> nerf
-ChangeStats("meat_dried", 18.75, 15, 20)
+-- ChangeStats("meat_dried", 18.75, 15, 20)
 
 -- Drumstick
 -- 12.5, -10, 0 -> nerf
-ChangeStats("drumstick", 9.375, -10, 0)
+-- ChangeStats("drumstick", 9.375, -10, 0)
 
 -- Frog Legs
 -- 12.5, -10, 0 -> nerf
-ChangeStats("froglegs", 9.375, 10, 0)
+-- ChangeStats("froglegs", 9.375, -10, 0)
 
 -- Morsel
 -- 12.5, -10, 0 -> nerf
-ChangeStats("smallmeat", 9.375, 10, 0)
+-- ChangeStats("smallmeat", 9.375, -10, 0)
 
 -- Small Jerky
 -- 12.5, 10, 8 -> nerf
-ChangeStats("smallmeat_dried", 9.375, 10, 8)
+-- ChangeStats("smallmeat_dried", 12.5, 10, 8)
 
 
 
@@ -231,7 +235,7 @@ ChangeStats("pepper_seeds", 2.375, 0, 0)
 ChangeStats("garlic_seeds", 2.375, 0, 0)
 
 
----- Dishes ----
+---- Dishes ---- https://dontstarve.wiki.gg/wiki/Dishes ----
 
 -- Asparagus Soup
 -- 18.75, 5, 20 -> no changes
@@ -239,51 +243,78 @@ ChangeStats("asparagussoup", 18.75, 5, 20)
 
 -- Banana Pop
 -- 12.5, 33, 20 -> nerf -> reason: allowing ice
-ChangeStats("bananapop", 12.5, 33, 0)
+ChangeStats("bananapop", 12.5, 33, 3)
 
 -- Banana Shake
 -- 25, 33, 8 -> buff -> reason: unused
-ChangeStats("bananajuice", 25, 33, 20)
+ChangeStats("bananajuice", 25, 15, 40)
 
 -- Bunny Stew
 -- 37.5, 5, 20 -> nerf -> reason: allowing ice
 ChangeStats("bunnystew", 12.5, 5, 20)
+-- (meat < 1) and (frozen >=2) -> buff -> reason: fallback for meat+ice dishes
+for _, recipe_table in pairs(GLOBAL.require("cooking").recipes) do
+    if recipe_table.bunnystew then
+        recipe_table.bunnystew.test = function(cooker, names, tags)
+            return (tags.meat and tags.meat > 0) 
+                and (tags.frozen and tags.frozen > 0) 
+                and (not tags.inedible)
+        end
+    end
+end
 
 -- Ceviche
 -- 25, 5, 20 -> buff -> reason: unused
 ChangeStats("ceviche", 37.5, 5, 40)
 
+-- Dragonpie
+-- 75, 5, 40 -> nerf -> reason: OP
+ChangeStats("dragonpie", 25, 5, 40)
+
 -- Fancy Spiralled Tubers
--- 37.5, 15, 3 -> nerf -> reason: OP + potato nerf
+-- 37.5, 15, 3 -> nerf -> reason: potato nerf
 ChangeStats("potatotornado", 18.75, 15, 3)
 
 -- Fish Tacos
--- 37.5, 5, 20 -> rebalance -> reason: unused
-ChangeStats("fishtacos", 18.75, 15, 20)
+-- 37.5, 5, 20 -> buff -> reason: unused
+ChangeStats("fishtacos", 37.5, 15, 40)
+
+-- Fishsticks 
+-- 37.5, 5, 40 -> nerf -> reason: allowing twigs
+ChangeStats("fishsticks", 37.5, 5, 20)
 
 -- Fist Full of Jam
 -- 37.5, 5, 3 -> nerf -> reason: allowing ice
 ChangeStats("jammypreserves", 18.75, 5, 3)
 
 -- Frozen Banana Daiquiri
--- 18.75, 15, 30 -> buff -> reason: unused
-ChangeStats("frozenbananadaiquiri", 18.75, 15, 40)
+-- 18.75, 15, 30 -> nerf
+ChangeStats("frozenbananadaiquiri", 12.5, 33, 20)
 
 -- Meaty Stew
 -- 150, 5, 12 -> nerf -> reason: OP + allowing ice
 ChangeStats("bonestew", 92.5, 5, 12)
 
 -- Monster Lasagna
--- 37.5, -20, -20 -> buff -> reason: global monster meat ban
-ChangeStats("monsterlasagna", 62.5, -20, -20)
+-- 37.5, -20, -20 -> no changes
+ChangeStats("monsterlasagna", 37.5, -20, -20)
+-- (priority = 10) and (monster >= 2) -> buff -> reason: fallback for any monster meat dishes
+for _, recipe_table in pairs(GLOBAL.require("cooking").recipes) do
+    if recipe_table.monsterlasagna then
+        recipe_table.monsterlasagna.priority = 99
+        recipe_table.monsterlasagna.test = function(cooker, names, tags)
+            return (tags.monster or 0) >= 1
+        end
+    end
+end
 
 -- Pumpkin Cookie
 -- 37.5, 15, 0 -> rebalance -> reason: unused
-ChangeStats("pumpkincookie", 18.75, 33, 0)
+ChangeStats("pumpkincookie", 18.75, 20, 0)
 
 -- Salsa Fresca
--- 25, 33, 3 -> nerf -> reason: allowing ice
-ChangeStats("salsa", 18.75, 33, 3)
+-- 25, 33, 3 -> rebalance
+ChangeStats("salsa", 18.75, 15, 20)
 
 -- Trail Mix
 -- 12.5, 5, 30 -> nerf -> reason: OP
@@ -321,10 +352,10 @@ local ICE_ALLOW_LIST = {
     mandrakesoup = true,
     bonestew = true,
     watermelonicle = true,
-    salsa = true,
     seafoodgumbo = true,
     sweettea = true,
     vegstinger = true,
+    monsterlasagna = true,
 }
 
 local TWIGS_ALLOW_LIST = {
@@ -337,6 +368,7 @@ local TWIGS_ALLOW_LIST = {
     powcake = true,
     beefalofeed = true,
     monsterlasagna = true,
+    shroombait = true,
 }
 
 local function ApplyCrockPotRestrictions()
@@ -367,27 +399,16 @@ end
 local recipes = cooking.recipes.cookpot
 local portable_recipes = cooking.recipes.portablecookpot
 
-local function OverrideLasagna(recipe_table)
-    if recipe_table and recipe_table.monsterlasagna then
-        recipe_table.monsterlasagna.priority = 99
-        recipe_table.monsterlasagna.test = function(cooker, names, tags)
-            return (tags.monster or 0) >= 1
-        end
-    end
-end
-
 if recipes then
     if recipes.shroombait then
         recipes.shroombait.priority = 100
     end
-    OverrideLasagna(recipes)
 end
 
 if portable_recipes then
     if portable_recipes.monstertartare then
         portable_recipes.monstertartare.priority = 100
     end
-    OverrideLasagna(portable_recipes)
 end
 
 ApplyCrockPotRestrictions()
@@ -401,7 +422,7 @@ local function ModifyBeeLoot(inst)
         inst.components.lootdropper:SetChanceLootTable(nil)
         inst.components.lootdropper:SetLoot({})
         inst.components.lootdropper.chanceloots = {}
-        -- Add 25% Stinger drop
+        -- Add 5% Stinger drop
         inst.components.lootdropper:AddChanceLoot("stinger", 0.05)
     end
 end
